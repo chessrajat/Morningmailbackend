5cb3e3e279291338dc7ff2ed18a2f49fe66ed609
chessrajat <raat66rat@gmail.com>
2023-05-03 11:52:18 +0530

    added url for index.html

--- morningmail/urls.py
+++ morningmail/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/', include("users.urls")),
     path('api/v1/email/', include("dashboard.urls")),
+    re_path(r"^$", TemplateView.as_view(template_name="index.html")),
+    re_path(r"^(?:.*)/?$", TemplateView.as_view(template_name="index.html"))
 ]
